fb941a430e1ccf8a0e615dc3fd5aa82e691204b3
Subham Luitel <amk1004106@student.hamk.fi>
2025-04-27 19:52:26 +0300

    Added Thread for Obstacle Avoidance Logic

--- src/multisensor/UltrasonicSensor.java
+++ src/multisensor/UltrasonicSensor.java
@@ -1,8 +1,10 @@
 package multisensor;
 
+import lejos.hardware.Button;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
 import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 
 public class UltrasonicSensor {
     private static EV3UltrasonicSensor ultrasonic = new EV3UltrasonicSensor(SensorPort.S2);
@@ -16,5 +18,34 @@ public class UltrasonicSensor {
 
     public static void close() {
         ultrasonic.close();
+    }
+}
+
+public class UltrasonicSensor implements Runnable {
+    private static EV3UltrasonicSensor ultrasonic = new EV3UltrasonicSensor(SensorPort.S2);
+    private static SampleProvider distanceProvider = ultrasonic.getDistanceMode();
+    private static float[] sample = new float[distanceProvider.sampleSize()];
+    private static Thread avoidanceThread;
+
+    public static void startAvoidance() {
+        avoidanceThread = new Thread(new UltrasonicSensor());
+        avoidanceThread.start();
+    }
+
+    public static float getDistance() {
+        distanceProvider.fetchSample(sample, 0);
+        return sample[0];
+    }
+
+    public static void close() {
+        ultrasonic.close();
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.interrupted() && !Button.ESCAPE.isDown()) {
+            float distance = getDistance();
+            Delay.msDelay(100);
+        }
     }
 }
