5cf37b0f35b70372aa9a497b855d27b0991570f6
Puran Karki <xettripuran81@gmail.com>
2025-04-27 18:28:34 +0300

    Set up LineFollower class and adding motor speed constants and variables

---
+++ src/multisensor/LineFollower.java
@@ -0,0 +1,15 @@
+package multisensor;
+
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+public class LineFollower implements Runnable {
+    public void run() {
+        int normalSpeed = 200;
+        int maxMotorSpeed = 900;
+        int minMotorSpeed = 0;
+        float proportionalGain = 700;
+
+    }
+}
